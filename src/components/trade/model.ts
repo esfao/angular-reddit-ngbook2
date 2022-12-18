c0df2863d4d4bb933bdeb99d1b38d496f503bd2a
esfao <101914102+esfao@users.noreply.github.com>
2022-12-18 17:35:03 +0000

    travis file

--- src/components/trade/model.ts
+++ src/components/trade/model.ts
@@ -9,4 +9,38 @@ import {Actions} from "./intent";
 
 const defaultState: State = {
     currentPrice: 0,
-    histo
+    histories: [],
+    ifdocoOrder: new IFDOCOrder(10000, 50),
+    isOrdering: false,
+    orders: [],
+    position: new Position([]),
+    price: 0,
+    size: 0,
+    stopOrders: [],
+};
+
+export const model = (actions: Actions): Stream<Reducer<State>> => {
+    const defaultReducer$ = Stream.of((state: State) => state || defaultState);
+
+    const cancelOrdersReducer$ = actions.onCancelOrders$
+        .map((_) => (state: State) => ({ ...state, orders: [] as Order[], stopOrders: [] as StopOrder[] }));
+
+    const currentPriceReducer$ = actions.onExecutionCreated$
+        .map((execution: any) => (state: State) => {
+            if (state.boardComponentState) { state.boardComponentState.board.remove(execution.side, execution.price); }
+            return { ...state, currentPrice: execution.price };
+        });
+
+    const historyReducer$ = actions.onHistoryCreated$
+        .map((history) => (state: State) => ({ ...state, histories: [history].concat(state.histories)}));
+
+    const isOrderingReducer$ = Stream.merge(
+        actions.onClickMarketBuyButton$.mapTo(true),
+        actions.onClickMarketSellButton$.mapTo(true),
+        actions.onClickLimitBuyButton$.mapTo(true),
+        actions.onClickLimitSellButton$.mapTo(true),
+        actions.onClickClearButton$.mapTo(true),
+        actions.onHistoryCreated$.mapTo(false),
+    ).map((isOrdering) => (state: State) => ({ ...state, isOrdering }));
+
+    const ordersRed
