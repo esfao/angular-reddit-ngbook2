0f6ab52a7c5ec0bb56557ab7e02713ed22c1c7d3
esfao <101914102+esfao@users.noreply.github.com>
2023-01-01 09:03:58 +0000

    more hacks

--- src/components/trade/request.ts
+++ src/components/trade/request.ts
@@ -27,4 +27,40 @@ export const request = (actions: Actions, state$: Stream<State>): Stream<string 
         .compose(sampleCombine(state$))
         .map(([_, state]) => marketOrder(state.size, "BUY"));
 
-    const market
+    const marketSell = actions.onClickMarketSellButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) => marketOrder(state.size, "SELL"));
+
+    const limitBuy = actions.onClickLimitBuyButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) => limitOrder(state.price, state.size, "BUY"));
+
+    const limitSell = actions.onClickLimitSellButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) => limitOrder(state.price, state.size, "SELL"));
+
+    const clear = actions.onClickClearButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) => marketOrder(state.position.size, state.position.side === "BUY" ? "SELL" : "BUY"));
+
+    const clearOrders = actions.onClickClearOrderButton$
+        .map((_) => cancelOrders());
+
+    const ifdocoBuy = actions.onClickIFDOCOBuyButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) =>
+            ifdocoOrder(
+                state.ifdocoOrder.buyProfitLine(state.currentPrice),
+                state.ifdocoOrder.buyLossLine(state.currentPrice),
+                state.size,
+                "BUY",
+            ),
+        );
+
+    const ifdocoSell = actions.onClickIFDOCOSellButton$
+        .compose(sampleCombine(state$))
+        .map(([_, state]) =>
+            ifdocoOrder(
+                state.ifdocoOrder.sellProfitLine(state.currentPrice),
+                state.ifdocoOrder.sellLossLine(state.currentPrice),
+         
