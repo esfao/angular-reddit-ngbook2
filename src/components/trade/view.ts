0d3ea446bb562633006f94e8531b0c1214c9e7b0
esfao <101914102+esfao@users.noreply.github.com>
2023-01-29 06:16:35 +0000

    tipc: clear 'next'-pointer of message fragments before reassembly

--- src/components/trade/view.ts
+++ src/components/trade/view.ts
@@ -1,2 +1,29 @@
 import {button, div, h4, hr, input, label, p, span, VNode} from "@cycle/dom";
-import Stream from "
+import Stream from "xstream";
+import sampleCombine from "xstream/extra/sampleCombine";
+import {State} from "./index";
+
+export const view = (state$: Stream<State>, boardComponentDOM$: Stream<VNode>, summaryComponentDOM$: Stream<VNode>) =>
+    state$
+        .compose(sampleCombine(boardComponentDOM$))
+        .compose(sampleCombine(summaryComponentDOM$))
+        .map(([[state, boardComponentDOM], summaryComponentDOM]) =>
+            div(".trade", [
+                summaryComponentDOM,
+                boardComponentDOM,
+                div(".order", [
+                    h4(".sub-title", "Size"),
+                    div(".size", [
+                        input("#size-input", { attrs: { value: state.size }}),
+                    ]),
+                    hr(),
+                    h4(".sub-title", "Market"),
+                    div(".market-order-buttons.order-buttons", [
+                        button(".sell-button", { attrs: { disabled: state.isOrdering || state.size === 0 } }, "Sell"),
+                        button(".buy-button", { attrs: { disabled: state.isOrdering || state.size === 0 } }, "Buy"),
+                    ]),
+                    hr(),
+                    h4(".sub-title", "Limit"),
+                    div(".limit-order-buttons.order-buttons", [
+                        input("#price-input", { attrs: { value: state.price }}),
+                        button(".sell-button", { attrs: { disabled: state.isOrdering || state.size === 0 
