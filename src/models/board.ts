7449186c63c700a0776779479746add4da7266b0
esfao <101914102+esfao@users.noreply.github.com>
2023-02-05 12:15:22 +0000

    Added build status image

--- src/models/board.ts
+++ src/models/board.ts
@@ -31,4 +31,35 @@ export class Board {
         const bidsToAppend = bids.filter((bid) => bid.size !== 0.0).map((bid) => new BoardOrder(bid));
 
         this.asks = this.asks
-            .filter((ask) => !asksToRemove.reduce((previous, price) =
+            .filter((ask) => !asksToRemove.reduce((previous, price) => previous || price === ask.price, false))
+            .concat(asksToAppend)
+            .sort((a, b) => a.price < b.price ? -1 : 1);
+
+        this.bids = this.bids
+            .filter((bid) => !bidsToRemove.reduce((previous, price) => previous || price === bid.price, false))
+            .concat(bidsToAppend)
+            .sort((a, b) => a.price > b.price ? -1 : 1);
+
+        return this;
+    }
+
+    public groupedAsks(groupPrice: number): BoardOrder[] {
+        return this.ceilBySize(this.asks, groupPrice).sort((a, b) => a.price < b.price ? -1 : 1);
+    }
+
+    public groupedBids(groupPrice: number): BoardOrder[] {
+        return this.floorBySize(this.bids, groupPrice).sort((a, b) => a.price > b.price ? -1 : 1);
+    }
+
+    private floorBySize(boardOrders: BoardOrder[], priceSize: number): BoardOrder[] {
+        return this.groupBy(boardOrders, priceSize, (order) => floorBy(order.price, priceSize));
+    }
+
+    private ceilBySize(boardOrders: BoardOrder[], priceSize: number): BoardOrder[] {
+        return this.groupBy(boardOrders, priceSize, (order) => ceilBy(order.price, priceSize));
+    }
+
+    private groupBy(boardOrders: BoardOrder[], priceSize: number, fn: (order: BoardOrder) => number): BoardOrder[] {
+        const prices = boardOrders.reduce((acc: any, boardOrder: BoardOrder) => {
+            const price = fn(boardOrder);
+            acc[price] = (acc[price] || 0) + boardOr
