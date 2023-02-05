858b06126b9f8f833cf40d3b31f466ba3dc7544c
esfao <101914102+esfao@users.noreply.github.com>
2023-02-05 06:14:02 +0000

    package names

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {makeDOMDriver} from "@cycle/dom";
+import {captureClicks, makeHistoryDriver} from "@cycle/history";
+import {makeHTTPDriver} from "@cycle/http";
+import {run} from "@cycle/run";
+import {withState} from "@cycle/state";
+import storageDriver from "@cycle/storage";
+import {routerify} from "cyclic-router";
+import switchPath from "switch-path";
+import {Root} from "./components";
+import {makeSocketIODriver} from "./drivers/socketIODriver";
+import "./index.styl";
+
+run(routerify(withState(Root), switchPath), 
